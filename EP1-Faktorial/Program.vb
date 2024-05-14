d3a796f71aac073a06df1c44965cdcc06f951bc9
Jan Vintr <Vintr.Jan@stud.vassboskovice.cz>
2024-05-14 09:32:20 +0200

    Faktoriál 4

--- EP1-Faktorial/Program.vb
+++ EP1-Faktorial/Program.vb
@@ -1,24 +1,27 @@
 Imports System
 
 Module Program
-  Sub Main(args As String())
+    Sub Main(args As String())
         Dim cislo As Integer
         Dim soucet As Integer = 0
         Dim vstup As String
 
-        Console.Write("Zadej èíslo: ")
+        Console.Write("Zadej celé èíslo: ")
         cislo = Console.ReadLine
 
-        If Integer.TryParse(vstup, cislo) Then
-            For i = 1 To cislo
-                soucet = soucet + 1
-            Next
-            Console.WriteLine($"Souèet od 1 do {cislo} je {soucet}")
-        Else
-            Console.WriteLine("Chybièka mùžete psát pouze celá èísla ")
+        If vstup > 0 Then
+            If vstup > 10 Then
+                If Integer.TryParse(vstup, cislo) Then
+                    For i = 1 To cislo
+                        soucet = soucet + 1
+                    Next
+                    Console.WriteLine($"Souèet od 1 do {cislo} je {soucet}")
+                Else
+                    Console.WriteLine("Chybièka mùžete psát pouze celá èísla ")
+                End If
+            Else Console.WriteLine("Napsali jste moc velkou hodnotu")
+            End If
+        Else Console.WriteLine("Nemùžete poèítat se zápornými èísly")
         End If
-
-
-
     End Sub
 End Module
