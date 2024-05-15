cd9cc46dfcbd50afa678043568713280181069ff
Tomáš Čížek <Cizek.Tomas@stud.vassboskovice.cz>
2024-05-15 09:08:35 +0200

    musí být číslo

--- EP1-Faktorial/Program.vb
+++ EP1-Faktorial/Program.vb
@@ -1,7 +1,21 @@
 Imports System
 
-Module Program
-  Sub Main(args As String())
-    Console.WriteLine("Hello World!")
-  End Sub
-End Module
+
+Imports System
+    Imports System.Numerics
+    Imports System.Security.Cryptography
+
+    Module Program
+        Sub Main(args As String())
+            Dim cislo As Object
+            Dim vysledek As Integer = 1
+
+        Console.WriteLine("Zadej èíslo")
+        cislo = Console.ReadLine
+
+
+            If Not IsNumeric(cislo) Then
+            Console.WriteLine(" Vstup musí být èíslo ")
+            Console.ReadKey()
+                End
+            End If
